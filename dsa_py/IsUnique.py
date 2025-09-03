35de3a536599f41f45c4733b9b4767a40e35f943
Igbanesi michael <eshimi090@gmail.com>
2025-09-03 23:10:04 +0100

    Updated IsUnique file

--- dsa_py/IsUnique.py
+++ dsa_py/IsUnique.py
@@ -1,1 +1,15 @@
-print('Hello')
+#'IsUnique with Dsa'
+#def isUnique(s: str) -> bool:
+ #   return len(set(s)) == len(s)
+
+
+#print(isUnique('hello')) */
+
+'Without dsa'
+def isUniqueNoDsa(s: str) -> bool:
+    chars = sorted(s)
+    for i in range(1, len(chars)):
+        if chars[i] == chars[i - 1]:
+            return False
+    return True
+print(isUniqueNoDsa('hate'))
